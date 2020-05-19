1e3534512fa2b98e61fc6bbdf4a0ead29f736e24
putheakhem <puthea.khem@gmail.com>
2020-05-19 22:41:20 +0700

    pass data to view

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/test', 'HomeController@showTest');
 Route::get('/', 'HomeController@index');
 Route::get('/aboutus', 'HomeController@aboutus');
+Route::get('/posts', 'PostController@index');
